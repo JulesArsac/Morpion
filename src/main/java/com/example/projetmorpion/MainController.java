fefaee31579b8b91d0a0166241d678da73a5713d
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2023-03-06 18:42:38 +0100

    Creating difficulty window

--- src/main/java/com/example/projetmorpion/MainController.java
+++ src/main/java/com/example/projetmorpion/MainController.java
@@ -4,9 +4,14 @@ import ai.MultiLayerPerceptron;
 import ai.SigmoidalTransferFunction;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
 
@@ -78,5 +83,16 @@ public class MainController {
             e.printStackTrace();
             System.exit(-1);
         }
+    }
+
+    @FXML
+    void openDifficultySettings() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("difficultySettings.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 200);
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        stage.setTitle("Difficulty Settings");
+        stage.setScene(scene);
+        stage.show();
     }
 }
