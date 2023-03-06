24090f42df95e9d9e807f81618437479af3d3f73
Jules <jules.arsac@alumni.univ-avignon.fr>
2023-03-06 14:08:55 +0100

    Refactor + début d'interface

--- src/main/java/com/example/projetmorpion/MainApplication.java
+++ src/main/java/com/example/projetmorpion/MainApplication.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("trainingView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Morpion");
         stage.setScene(scene);
         stage.show();
